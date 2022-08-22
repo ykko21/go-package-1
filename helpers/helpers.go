46a603c1a291bf098b4033f72fcf79c418b86603
ykko21 <ykko21@gmail.com>
2022-08-21 23:05:53 -0400

    init

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,7 +1,18 @@
 package helpers
 
+import (
+	"math/rand"
+	"time"
+)
+
 type Customer struct {
 	CustomerId      string
 	FirstName       string
 	CellPhoneNumber string
+}
+
+func RandomeNumber(n int) int {
+	rand.Seed(time.Now().UnixNano())
+	value := rand.Intn(n)
+	return value
 }
--- main.go
+++ main.go
@@ -7,7 +7,27 @@ import (
 	"github.com/ykko21/go_package_1/helpers"
 )
 
+const numPool = 10
+
+func CalculateValue(intChan chan int) {
+	randNum := helpers.RandomeNumber(numPool)
+	intChan <- randNum
+}
+
 func main() {
+	intChan := make(chan int)
+	defer close(intChan)
+	go CalculateValue(intChan)
+
+	num := <-intChan //Listing for the channel
+	log.Println(num)
+}
+
+func t2() { //channel test
+
+}
+
+func t1() {
 	log.Println("Hello")
 	var cust helpers.Customer
 	cust.FirstName = "Peter"
